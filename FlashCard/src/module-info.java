285a9587c2d3335bc4716e6460928d75a83e578f
dao24092004 <134945882+dao24092004@users.noreply.github.com>
2024-10-26 21:34:35 +0700

    Initial commit

---
+++ FlashCard/src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module FlashCard {
+	requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
+    requires java.desktop;
+    requires com.miglayout.swing;  // Sử dụng MigLayout
+    requires org.jdesktop.animation.timing;
+	requires timingframework;  // Sử dụng Animator
+}
